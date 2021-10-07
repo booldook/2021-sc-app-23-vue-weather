b293e57426c42d37cdf170c227e683614e3c7420
booldook <booldook@gmail.com>
2021-10-07 15:43:25 +0900

    _variables color scheme

---
+++ src/components/inc/Header.vue
@@ -0,0 +1,17 @@
+<template>
+  <header class="header-wrapper">
+
+  </header>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang="scss" scoped>
+.header-wrapper {
+  
+}
+</style>
