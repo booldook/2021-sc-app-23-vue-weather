ca0bd10022692bab12b6de025075961fcc1399ff
booldook <booldook@gmail.com>
2021-10-08 16:29:19 +0900

    Daily Component 작업 전

--- src/views/Daily.vue
+++ src/views/Daily.vue
@@ -1,24 +1,40 @@
 <template>
   <div class="daily-wrapper">
     <h2 class="city-wrap">
-      {{ GET_DAILY }}
+      {{ city }}
     </h2>
     <div class="img-wrap">
-      <img src="" alt="">
+      <img :src="src" alt="">
     </div>
     <div class="temp-wrap">
-      {{  }}
+      {{ temp }}
     </div>
   </div>
 </template>
 
 <script>
 import { mapGetters } from 'vuex'
+import { getIcon } from '../modules/util'
 
 export default {
   name: 'Daily',
   computed: {
-    ...mapGetters(['GET_COORDS', 'GET_DAILY'])
+    ...mapGetters(['GET_COORDS', 'GET_DAILY']),
+    city: function () {
+      return (this.GET_DAILY.cod === 200)
+        ? `${this.GET_DAILY.name}, ${this.GET_DAILY.sys.country}`
+        : ''
+    },
+    src: function () {
+      return (this.GET_DAILY.cod === 200)
+        ? getIcon(this.GET_DAILY.weather[0].icon)
+        : 'https://via.placeholder.com/50x50?text=No+Icon'
+    },
+    temp: function () {
+      return (this.GET_DAILY.cod === 200)
+        ? this.GET_DAILY.main.temp + '℃'
+        : ''
+    }
   },
   watch: {
     GET_COORDS: function (v, ov) {
@@ -35,5 +51,8 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-
+.daily-wrapper {
+  @include flex($h: center, $v: center);
+  @include flexCol;
+}
 </style>
