d3a35ac223902cd46662a12236006faf751783f2
booldook <booldook@gmail.com>
2021-10-08 15:51:07 +0900

    vuex init

--- src/views/Daily.vue
+++ src/views/Daily.vue
@@ -1,5 +1,33 @@
 <template>
-  <div class="daily">
-    <h1>Daily</h1>
+  <div class="daily-wrapper">
+    <div>
+      {{ GET_COORDS.lat }} / {{ GET_COORDS.lon }}
+    </div>
   </div>
 </template>
+
+<script>
+import { mapGetters } from 'vuex'
+
+export default {
+  name: 'Daily',
+  computed: {
+    ...mapGetters(['GET_COORDS', 'GET_DAILY'])
+  },
+  watch: {
+    GET_COORDS: function (v, ov) {
+      this.$store.dispatch('ACT_DAILY', v)
+    },
+    GET_DAILY: function (v, ov) {
+      console.log(v)
+    }
+  },
+  created () {
+    this.$store.dispatch('ACT_COORDS')
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
