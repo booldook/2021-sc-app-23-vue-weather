3e77d2008f19d7cf3b545db28bae23db0df814a6
booldook <booldook@gmail.com>
2021-10-12 11:19:32 +0900

    ListWrap 작업중

---
+++ src/components/ListWrap.vue
@@ -0,0 +1,16 @@
+<template>
+  <ul class="list-wrap">
+
+  </ul>
+</template>
+
+<script>
+export default {
+  name: 'ListWrap',
+  props: ['list']
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
