79b3015f05c7346e4301ad5ecfe64049de806a7d
booldook <booldook@gmail.com>
2021-10-13 11:23:11 +0900

    vuex에서 daily, days, city 걷어냄

--- src/store/actions.js
+++ src/store/actions.js
@@ -1,6 +1,4 @@
 import { apiCoords } from '../api/api-coords'
-import apiWeather from '../api/api-weather'
-import apiCity from '../api/api-city'
 import { DEF_COORDS } from '../modules/common'
 
 const ACT_COORDS = async ({ commit }, v) => {
@@ -16,19 +14,4 @@ const ACT_COORDS = async ({ commit }, v) => {
   }
 }
 
-const ACT_DAILY = async ({ commit }, v) => {
-  const { data } = await apiWeather('daily', v)
-  commit('MUT_DAILY', data)
-}
-
-const ACT_DAYS = async ({ commit }, v) => {
-  const { data } = await apiWeather('days', v)
-  commit('MUT_DAYS', data)
-}
-
-const ACT_CITY = async ({ commit }) => {
-  const { data } = await apiCity()
-  commit('MUT_CITY', data.city)
-}
-
-export default { ACT_COORDS, ACT_DAILY, ACT_DAYS, ACT_CITY }
+export default { ACT_COORDS }
